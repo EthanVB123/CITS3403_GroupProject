fb8b678b28afb94b67fe65153ed338fe5fda4dce
EthanVB123 <ethanvanbruchem@gmail.com>
2025-04-10 19:49:24 +0800

    Dynamically added id tags to each cell element so that we can tell which cell was clicked when puzzle.onclick was called

--- solvePuzzle.js
+++ solvePuzzle.js
@@ -1,3 +1,18 @@
 let puzzleSize = [5, 5]; // [rows, cols]
 let rowClues = [[5],[1],[5],[1],[5]];
 let colClues = [[3,1],[1,1,1],[1,1,1],[1,1,1],[1,3]];
+
+document.addEventListener("DOMContentLoaded", () => {
+    const puzzle = document.getElementById("puzzle");
+    let cellIndex = 0;
+    const cells = document.getElementsByClassName("cell");
+    for(let cell of cells) {
+        cell.id = `cell${cellIndex}`;
+        cellIndex++;
+    };
+
+    puzzle.addEventListener("click", event => {
+        const itemClicked = event.target;
+        console.log(`Puzzle element ${itemClicked.id} clicked`);
+    });
+})
