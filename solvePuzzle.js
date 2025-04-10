3939a4788109b72304e958c659b861a568f669aa
EthanVB123 <ethanvanbruchem@gmail.com>
2025-04-10 22:33:31 +0800

    Clicking on a cell now toggles whether or not it is shaded, and an array shadedCells of which arrays are shaded has been created

--- solvePuzzle.js
+++ solvePuzzle.js
@@ -2,6 +2,7 @@ let puzzleSize = [5, 5]; // [rows, cols]
 let rowClues = [[5],[1],[5],[1],[5]];
 let colClues = [[3,1],[1,1,1],[1,1,1],[1,1,1],[1,3]];
 
+let shadedCells = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
 document.addEventListener("DOMContentLoaded", () => {
     const puzzle = document.getElementById("puzzle");
     let cellIndex = 0;
@@ -14,5 +15,14 @@ document.addEventListener("DOMContentLoaded", () => {
     puzzle.addEventListener("click", event => {
         const itemClicked = event.target;
         console.log(`Puzzle element ${itemClicked.id} clicked`);
+        const itemnum = parseInt(itemClicked.id.substr(4));
+        const row = Math.trunc(itemnum/5);
+        const col = itemnum % 5;
+        if (shadedCells[row][col]) {
+            shadedCells[row][col] = 0
+        } else {
+            shadedCells[row][col] = 1
+        }
+        itemClicked.classList.toggle("shaded");
     });
 })
