318f5a07115f14a158e0f34a85e77dee86851065
EthanVB123 <ethanvanbruchem@gmail.com>
2025-04-19 22:21:51 +0800

    Added all routes agreed to be in the project into routes.py, with a stub directing to test.html and an argument saying which function they were added from

--- app/routes.py
+++ app/routes.py
@@ -2,5 +2,41 @@ from flask import render_template
 from . import app
 
 @app.route("/")
-def test():
-    return render_template('test.html', argument="Hello World!")
+def homePage():
+    return render_template('test.html', argument="This should be the home page.")
+
+@app.route("/login")
+def loginPage():
+    return render_template('test.html', argument="This should be the login page.")
+
+@app.route("/profile/<username>") # <username> is a dynamic element.
+def userProfile(username):
+    return render_template('test.html', argument=f"This should display the profile of user {username}.")
+
+@app.route('/newpuzzle')
+def puzzleCreationLandingPage():
+    return render_template('test.html', argument="This should display the createPuzzle screen.")
+
+@app.route('/friends/<username>')
+def displayFriendsPage(username):
+    return render_template('test.html', argument=f"This should display the friends list of user {username}.")
+
+@app.route('/puzzleselect')
+def puzzleSelect():
+    return render_template('test.html', argument="This should be the landing page for Puzzle Select.")
+
+@app.route('/puzzleselect/<username>')
+def puzzleSelectFromUser(username):
+    return render_template('test.html', argument=f"This should display puzzles from user {username}.")
+
+@app.route('/puzzleselect/difficulty/<difficulty>')
+def puzzleSelectFromDifficulty(difficulty):
+    return render_template('test.html', argument=f"This should display puzzles with difficulty {difficulty}.")
+
+@app.route('/puzzle/<int:puzzleid>')
+def solvePuzzle(puzzleid):
+    return render_template('test.html', argument=f"This should solve puzzle with ID {puzzleid}.")
+
+@app.route('/puzzle/new')
+def puzzleEditor():
+    return render_template('test.html', argument="This should allow the user to make their own puzzle.")
