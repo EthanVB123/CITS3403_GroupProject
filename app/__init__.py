fe0b300718f996ac8417f1a2a1ad6569aef73b50
EthanVB123 <ethanvanbruchem@gmail.com>
2025-04-19 22:06:34 +0800

    Added routes.py, and created a test route with a test argument that works

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
-
 app = Flask(__name__)
-
+from . import routes # this is the same thing as from app import routes, but we are in app so that is redundant
 if __name__ == '__main__':
     app.run()
