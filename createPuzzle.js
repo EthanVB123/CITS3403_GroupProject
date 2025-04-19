f2f80c119f3f2245d73ea0718eb8ab597d5dccc4
EthanVB123 <ethanvanbruchem@gmail.com>
2025-04-19 18:05:56 +0800

    Overrided the default form submission behaviour, added form submission event listener with submitForm event which reads the inputs from each field and console.log()s them

--- createPuzzle.js
+++ createPuzzle.js
@@ -1,1 +1,14 @@
 // stub for now so that link from createPuzzle.html isn't broken
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("form");
+    form.addEventListener("submit", submitForm);
+});
+
+function submitForm(event) {
+    event.preventDefault();
+    console.log("Form submitted");
+    const puzzleName = document.getElementById("name").value;
+    const rows = document.getElementById("rows").value;
+    const columns = document.getElementById("columns").value;
+    console.log(`Puzzle name: ${puzzleName}, Num Rows: ${rows}, Num Columns: ${columns}`);
+}
