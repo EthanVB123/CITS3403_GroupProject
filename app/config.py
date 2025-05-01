8431d50995e85246d7f7824d22450fd482092cd5
EthanVB123 <ethanvanbruchem@gmail.com>
2025-05-01 11:33:23 +0800

    removed sensitive files from github, and changed secret key to prevent people from peeking at past commit to see the secret key

--- app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-default_db_location = 'sqlite:///' + os.path.join(basedir, 'puzzles.db')
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or default_db_location
-    SECRET_KEY = "very_secret" # not secure! in final codebase, should not make this public.
