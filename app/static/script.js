1412c644439190e7a41b90c13302dbbb17e4d955
jpengg <jeffreypeng7@gmail.com>
2025-05-08 01:17:16 +0800

    add some comments and formatting

--- app/static/script.js
+++ app/static/script.js
@@ -125,7 +125,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
         // Save our new user
         users.push({ email: email.toLowerCase(), username: username.toLowerCase(), password });
         localStorage.setItem("registeredUsers", JSON.stringify(users));
-        location.href = "/login"; //send them to the login page if succesfully completes registeration
+        window.location.href = "login.html"; //send them to the login page if succesfully completes registeration
         
       }
 
