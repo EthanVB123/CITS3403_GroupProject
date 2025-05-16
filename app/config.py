eb8efe6f0fc0f640c651ee70334e994ab6145e33
EthanVB123 <ethanvanbruchem@gmail.com>
2025-05-16 10:17:26 +0800

    Removed pycache from migrations, updated init.py and config.py to allow new databases to be added and removed secret key fallback from config

--- app/config.py
+++ app/config.py
@@ -1,13 +1,13 @@
 import os
  
-basedir = os.path.abspath(os.path.dirname(__file__))
-default_db_location = 'sqlite:///' + os.path.join(basedir, os.environ.get("FLASK_DATABASE_FILENAME",'puzzles.db'))
+
  
 class Config:
-    SECRET_KEY = os.environ.get("FLASK_SECRET_KEY", "supersecret") # not secure! in final codebase, should not make this public.
+    SECRET_KEY = os.environ.get("FLASK_SECRET_KEY") # not secure! in final codebase, should not make this public.
 
 class DeploymentConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or default_db_location
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, os.environ.get("FLASK_DATABASE_FILENAME"))
 
 class TestingConfig(Config):
     SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
